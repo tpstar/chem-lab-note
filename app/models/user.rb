fb01689df18686e780b477c53a3b98ac39c4dd13
Han Lee <ralphgoodwin@Ralphs-MacBook-Air.local>
2017-01-30 14:52:40 -0500

    implemeneted omniauth google auth 2

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,19 @@ class User < ApplicationRecord
   devise :omniauthable, :omniauth_providers => [:google_oauth2]
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+   def self.from_omniauth(access_token)
+     data = access_token.info
+     user = User.where(:email => data["email"]).first
+
+     # Uncomment the section below if you want users to be created if they don't exist
+     unless user
+         user = User.create(#name: data["name"],
+            email: data["email"],
+            password: Devise.friendly_token[0,20]
+         )
+     end
+     user
+ end
+
 end
