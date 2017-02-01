ba18b9400ed9c8920729fa9d98fb8966d1c8b365
Han Lee <ralphgoodwin@Ralphs-MacBook-Air.local>
2017-02-01 17:25:25 -0500

    add bootstrap

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,11 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  validates :first_name, :last_name, presence: true
+  has_many :reactions
+  has_many :chemicals
+
+
    def self.from_omniauth(access_token)
      data = access_token.info
      user = User.where(:email => data["email"]).first
