8c8c86951c38532b716598f75f7ae8fd810bd0e6
tpstar <hanlee12@gmail.com>
2017-02-09 00:31:46 -0500

    add angular-ui-router and angular-rails-templates

--- Gemfile
+++ Gemfile
@@ -18,6 +18,7 @@ gem 'sass-rails', '~> 5.0'
 gem 'uglifier', '>= 1.3.0'
 
 gem "bower-rails", "~> 0.11.0"
+gem 'angular-rails-templates'
 # Use CoffeeScript for .coffee assets and views
 # gem 'coffee-rails', '~> 4.2'
 # See https://github.com/rails/execjs#readme for more supported runtimes
--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,14 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users
   namespace :api do
     resources :reactions, :chemicals
   end
 
-  root 'api/reactions#index'
+  # root 'api/reactions#index'
+
+  root 'application#angular_home'
 
   # get '*unmatched_route', :to => 'reactions#index'
 end
