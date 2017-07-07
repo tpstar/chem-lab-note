81e5307d868379eb3ff96a02a0d2111643795add
Han Lee <ralphgoodwin@Ralphs-MacBook-Air.local>
2017-07-07 07:42:40 -0400

    add package.json to prepare for heroku buildpack

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170425201639) do
+ActiveRecord::Schema.define(version: 20170326042654) do
 
   create_table "chemicals", force: :cascade do |t|
     t.string   "name"
@@ -51,7 +51,6 @@ ActiveRecord::Schema.define(version: 20170425201639) do
     t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer  "upvotes"
   end
 
   create_table "users", force: :cascade do |t|
